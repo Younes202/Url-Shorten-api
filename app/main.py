597e958c6f6bb0c3666d97804b96af09879f4f38
Younes202 <younessghyer642@gmail.com>
2024-03-08 13:39:13 +0100

    set up the workflow corectly

--- app/main.py
+++ app/main.py
@@ -38,6 +38,3 @@ def redirect(short_link: str, db: Session = Depends(get_db)):
     return RedirectResponse(url=obj.original_url)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
