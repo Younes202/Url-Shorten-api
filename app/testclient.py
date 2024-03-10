82f6395db8fbdd7a49d4e1195af3b88799667769
Younes202 <younessghyer642@gmail.com>
2024-03-10 16:51:59 +0000

    set up ci

--- app/testclient.py
+++ app/testclient.py
@@ -8,13 +8,11 @@ def test_create_shortened_url():
     # Test creating a shortened URL
     url = "https://example.com"
     response = client.post("/urls/", json={"original_url": url})
-    # Ensure that the response status code is 404 for duplicate URL
-    assert response.status_code == 404
+    # Ensure that the response status code is 201 for successful creation
+    assert response.status_code == 201
+    short_link = response.json()["short_link"]
 
-
-def test_redirect():
-    with TestClient(app) as client:
-        short_link = "wHkltvM"
-        # Test redirection using the created short_link
-        redirect_response = client.get(f"/{short_link}")
-        assert redirect_response.status_code == 404
+    # Test redirection using the created short_link
+    redirect_response = client.get(f"/{short_link}")
+    # Ensure that the redirection response status code is 301 (redirect)
+    assert redirect_response.status_code == 301
